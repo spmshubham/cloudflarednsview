8498bcfe4246694a8a6f731f30e6f03cae25453e
spmshubham <32236774+spmshubham@users.noreply.github.com>
2023-11-05 22:39:45 +0530

    Update dnsrecord.sh

--- dnsrecord.sh
+++ dnsrecord.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Prompt the user for the Zone Identifier
-#read -p "Enter your Zone Identifier: " ZONE_IDENTIFIER
+read -p "Enter your Zone Identifier: " ZONE_IDENTIFIER
 
 # Prompt the user for the existing API Token
-#read -p "Enter your existing API Token: " API_TOKEN
+read -p "Enter your existing API Token: " API_TOKEN
 
 # Fetch DNS Records using the provided API Token
-DNS_RECORDS=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/a92b70b3d7d424d397e6ea82b0c10d54/dns_records" -H "Authorization: Bearer DdxSQ_T0iKvSbCkFNhZSwRKVKpoaqIP0BUZ3Pay4")
+DNS_RECORDS=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_IDENTIFIER/dns_records" -H "Authorization: Bearer $API_TOKEN")
 
 # Display DNS Records
 echo "DNS Records:"
 echo "$DNS_RECORDS" | jq .
-
